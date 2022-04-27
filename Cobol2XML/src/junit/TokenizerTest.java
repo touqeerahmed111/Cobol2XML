eb88f2d31f32e5bffd29cd4cf7bd0b99a0fbd4b4
Christopher <c.kearsley-brown@edu.salford.ac.uk>
2022-04-27 11:30:41 +0100

    Tokenizer Test

---
+++ Cobol2XML/src/junit/TokenizerTest.java
@@ -0,0 +1,45 @@
+package junit;
+
+import parse.tokens.Token;
+import parse.tokens.Tokenizer;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.Vector;
+
+import org.junit.Test;
+
+import cobol.CobolParser;
+
+public class TokenizerTest {
+
+	@Test
+	public void test() {
+		Tokenizer t = CobolParser.tokenizer();
+		t.setString("Hello world 2022");
+		Vector<Token> v = new Vector<Token>();
+		Token tok = null;
+		try {
+			tok = t.nextToken();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertTrue(tok.sval().equals("Hello"));
+		v.addElement(tok);
+		try {
+			tok = t.nextToken();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		assertFalse(tok.sval().equals("World"));
+		v.addElement(tok);
+		try {
+			tok = t.nextToken();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertTrue(tok.nval()==2022);
+		v.addElement(tok);
+	}
+}
